09c3a7a789e6c37a79fcea4c718f23f54efcd241
David Knoll <knolladavid@gmail.com>
2015-04-17 16:50:15 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StuffNThangs
+{
+    class Program
+    {
+        private static System.Timers.Timer aTimer;
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            aTimer = new System.Timers.Timer(5000);
+
+
+
+            Console.WriteLine(@"████████╗██╗  ██╗███████╗
+╚══██╔══╝██║  ██║██╔════╝
+   ██║   ███████║█████╗  
+   ██║   ██╔══██║██╔══╝  
+   ██║   ██║  ██║███████╗");
+            System.Threading.Thread.Sleep(1000);
+
+            Console.WriteLine(@"██╗    ██╗ █████╗ ██╗     ██╗  ██╗██╗███╗   ██╗ ██████╗ 
+██║    ██║██╔══██╗██║     ██║ ██╔╝██║████╗  ██║██╔════╝ 
+██║ █╗ ██║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗
+██║███╗██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║
+╚███╔███╔╝██║  ██║███████╗██║  ██╗██║██║ ╚████║╚██████╔╝
+ ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ");
+            System.Threading.Thread.Sleep(1000);
+
+            Console.WriteLine(@"██████╗ ███████╗██╗   ██╗
+██╔══██╗██╔════╝██║   ██║
+██║  ██║█████╗  ██║   ██║
+██║  ██║██╔══╝  ╚██╗ ██╔╝
+██████╔╝███████╗ ╚████╔╝ 
+╚═════╝ ╚══════╝  ╚═══╝  ");
+            System.Threading.Thread.Sleep(1000);
+            
+            
+            Console.WriteLine(Console.LargestWindowHeight);
+            Console.WriteLine(Console.LargestWindowWidth);
+            Console.Clear();
+            Console.WriteLine(@"███████╗████████╗██╗   ██╗███████╗███████╗          
+██╔════╝╚══██╔══╝██║   ██║██╔════╝██╔════╝          
+███████╗   ██║   ██║   ██║█████╗  █████╗            
+╚════██║   ██║   ██║   ██║██╔══╝  ██╔══╝            
+███████║   ██║   ╚██████╔╝██║     ██║               
+╚══════╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝               
+███╗   ██╗                                          
+████╗  ██║                                          
+██╔██╗ ██║                                          
+██║╚██╗██║                                          
+██║ ╚████║                                          
+╚═╝  ╚═══╝                                          
+████████╗██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ███████╗
+╚══██╔══╝██║  ██║██╔══██╗████╗  ██║██╔════╝ ██╔════╝
+   ██║   ███████║███████║██╔██╗ ██║██║  ███╗███████╗
+   ██║   ██╔══██║██╔══██║██║╚██╗██║██║   ██║╚════██║
+   ██║   ██║  ██║██║  ██║██║ ╚████║╚██████╔╝███████║
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝");
+            System.Threading.Thread.Sleep(1000);
+
+            Console.WriteLine("Presents a:\n BARELY LIVING PRODUCTION");
+            System.Threading.Thread.Sleep(1000);
+           
+                Console.WriteLine("Brought to in part by the people that enjoy living.");
+                System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("All the while avoiding those that would kill  that inalienable right. The Walking  Devs are everywhere, and as you make it through the open world of code, you stumble onto a Walking Dev, or Walker  ");
+            System.Threading.Thread.Sleep(1000);
+            
+            Console.WriteLine("");
+            Console.Write("");
+            Console.ReadKey();
+        
+       
+        }
+    }
+}
